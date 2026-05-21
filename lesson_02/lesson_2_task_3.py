3abc1702b9427029a3dc176d3c49a2b9868eb9d9
Ilya-Moryakov <deakon777@gmail.com>
2026-05-21 22:00:59 +0400

    Delete lesson_02 directory

--- lesson_02/lesson_2_task_3.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-def square(a):
-    s = a * a
-    return math.ceil(s)
--- lesson_02/lesson_2_task_7.py
+++
@@ -1,4 +0,0 @@
-numbers = range(18, 0, -4)
-numbers_list = list(numbers)
-print(type(numbers_list))
-print(numbers_list)
