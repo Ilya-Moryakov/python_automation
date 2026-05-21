292efe1acfb5846de9acd7f017414f08e453bf24
Ilya-Moryakov <deakon777@gmail.com>
2026-05-21 21:48:33 +0400

    Homework lesson3

---
+++ lesson_03/lesson_3_task_3.py
@@ -0,0 +1,19 @@
+from address import Address
+from mailing import Mailing
+
+sender = Address("101000", "Москва", "Арбат", "32", "1")
+recipient = Address("445000", "Тольятти", "Кудашева", "108", "45")
+
+mail = Mailing(
+    from_address = sender,
+    to_address = recipient,
+    cost = 100,
+    track = "MSK123"
+)
+
+print(
+    f"Отправление {mail.track} из {mail.from_address.index}, {mail.from_address.city}, "
+    f"{mail.from_address.street}, {mail.from_address.house} - {mail.from_address.apartment} "
+    f"в {mail.to_address.index}, {mail.to_address.city}, {mail.to_address.street}, "
+    f"{mail.to_address.house} - {mail.to_address.apartment}. Стоимость {mail.cost} рублей."
+)
