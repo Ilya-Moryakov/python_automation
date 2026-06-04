389a9d70a570b8623330855a514010e081a066cf
Ilya-Moryakov <deakon777@gmail.com>
2026-06-05 00:31:17 +0400

    Delete lesson_03 directory

--- lesson_03/address.py
+++
@@ -1,8 +0,0 @@
-class Address:
-
-    def __init__(self, индекс, город, улица, дом, квартира):
-        self.index = индекс
-        self.city = город
-        self.street = улица
-        self.house = дом
-        self.apartment = квартира
